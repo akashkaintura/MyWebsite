9f7d5df405dc030e9c5b029cda13382eb8e4009b
akashkoco <91880623+akashkoco@users.noreply.github.com>
2024-04-22 22:43:08 +0530

    added all the pages

---
+++ README.md
@@ -0,0 +1,21 @@
+# TechTuneTales
+
+TechTuneTales is a web application showcasing tech-related content including comedy, music, about section, and contact information. It's designed to provide a fun and informative experience for users interested in technology.
+
+## Features
+
+- **Comedy Section**: Enjoy hilarious tech jokes to lighten your mood.
+- **Music Section**: Groove to tech-inspired music parodies and catchy tunes.
+- **About Section**: Learn about the latest technology advancements and innovations.
+- **Contact Section**: Get in touch with the creators for inquiries or feedback.
+
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Acknowledgements
+
+- Thanks to [Netlify](https://www.netlify.com/) for providing hosting services.
+- Special thanks to contributors and open-source libraries used in this project.
+
---
+++ public/script.js
@@ -0,0 +1,43 @@
+// Function to change playlist
+function changePlaylist(playlistUrl) {
+    var iframe = document.getElementById('spotifyPlaylist');
+    iframe.src = "https://open.spotify.com/embed/playlist/" + playlistUrl;
+
+    document.querySelectorAll('.playlist-buttons button').forEach(btn => {
+        btn.classList.remove('active');
+    });
+    event.target.classList.add('active');
+}
+
+// Add event listeners to each button
+document.getElementById('viral50Btn').addEventListener('click', function() {
+    changePlaylist('37i9dQZEVXbK4NvPi6Sxit');
+});
+
+document.getElementById('top50GlobalBtn').addEventListener('click', function() {
+    changePlaylist('37i9dQZEVXbMDoHDwVN2tF');
+});
+
+document.getElementById('subhBtn').addEventListener('click', function() {
+    changePlaylist('37i9dQZF1E4yiJGrYbYs2x');
+});
+
+
+
+
+function changeBackgroundColor() {
+    const sections = document.querySelectorAll('section'); 
+    const body = document.body; 
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight; 
+
+        if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
+            const backgroundColor = section.getAttribute('data-background-color'); 
+            body.style.backgroundColor = backgroundColor;
+        }
+    });
+}
+
+window.addEventListener('scroll', changeBackgroundColor);
